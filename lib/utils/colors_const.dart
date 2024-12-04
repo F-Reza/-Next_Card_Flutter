902eff5ce26f1b44b250fb9f33ba7a22419f0b2c
Forhad Reza <forhadreza55@gmail.com>
2024-12-04 14:15:23 +0600

    Update - 4

---
+++ lib/utils/colors_const.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class AppColors {
+  static const Color cardBgLightColor = Color(0xFFF6F6F6);
+  static const Color cardBgColor = Color(0xFF1B1B1B);
+  static const Color colorE5D1B2 = Color(0xFFE5D1B2);
+  static const Color colorB58D67 = Color(0xFFB58D67);
+  static const Color colorF9EED2 = Color(0xFFF9EED2);
+  static const Color colorEFEFED = Color(0xFFEFEFED);
+}
